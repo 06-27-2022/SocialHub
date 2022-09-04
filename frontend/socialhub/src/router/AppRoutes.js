802f751442f0dd41b77d8653a33a23fe47967796
Muhammad Ali <aliadison@gmail.com>
2022-09-04 18:17:44 -0400

    Login feature

--- frontend/socialhub/src/router/AppRoutes.js
+++ frontend/socialhub/src/router/AppRoutes.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Route, Routes } from "react-router-dom";
 import { MainPage } from '../components/MainPage/MainPage';
-import Login from '../components/Login/Login';
+import {Login} from '../components/Login/Login'
 
 export const AppRoutes = () => (
   <Routes>
